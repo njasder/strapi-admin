b8d1b122861abecd0d790ac354a560137e464e15
claire <claire_duan@epam.com>
2021-02-02 10:43:12 +0800

    Add more logs for debugging

--- models/User.js
+++ models/User.js
@@ -23,43 +23,44 @@ module.exports = {
     async afterCreate(result) {
       try {
         if (result.subscribed) {
-          strapi.log.info('subscribe the user to outstanding topic while new model has subscribe as true');
+          console.log('subscribe the user to outstanding topic while new model has subscribe as true');
           await subscribe(result);
         } else {
-          strapi.log.info('un-subscribe the user to outstanding topic while new model has subscribe as false');
+          console.log('un-subscribe the user to outstanding topic while new model has subscribe as false');
           await subscribe(result);
         } 
       } catch (error) {
-        strapi.log.error(error)
+        console.log(error);
       }
 
     },
     async afterDelete(result) {
       try {
-        strapi.log.info('un-subscribe the user to outstanding topic after deleting');
+        console.log('un-subscribe the user to outstanding topic after deleting');
         await unsubscribe(result);
       } catch (error) {
-        strapi.log.error(error);
+        console.log(error);
       }
     },
   },
   async afterUpdate(result) {
     try {
       if (result.subscribed) {
-        strapi.log.info('subscribe the user to outstanding topic while model changed to have subscibe as true');
+        console.log('subscribe the user to outstanding topic while model changed to have subscibe as true');
         await subscribe(result);
       } else {
-        strapi.log.info('un-subscribe the user to outstanding topic while model changed  subscribe as false');
+        console.log('un-subscribe the user to outstanding topic while model changed  subscribe as false');
         await subscribe(result);
       }
     } catch (error) {
-      strapi.log.error(error)
+      console.log(error);
     }
   }
 };
 
 
 async function unsubscribe(result) {
+  console.log('Prepare for unsubscribe.');
   const mobile = result.bestContactNumber? result.bestContactNumber:result.username;
   const lambdaParams = {
     FunctionName: 'SNSMessageLambda',
@@ -78,11 +79,12 @@ async function unsubscribe(result) {
     // ingore the error because the lambda is a separate system
     // the error should not prevent, or affect the cms model lifecycle
     // only print the error message to console
-    strapi.log.error(error);
+    console.log(error);
   }
 }
 
 async function subscribe(result) {
+  console.log('Prepare for subscribe.');
   const mobile = result.bestContactNumber? result.bestContactNumber:result.username;
   const lambdaParams = {
     FunctionName: 'SNSMessageLambda',
@@ -100,7 +102,7 @@ async function subscribe(result) {
     // ingore the error because the lambda is a separate system
     // the error should not prevent, or affect the cms model lifecycle
     // only print the error message to console
-    strapi.log.error(error);
+    console.log(error);
   }
 }
 
